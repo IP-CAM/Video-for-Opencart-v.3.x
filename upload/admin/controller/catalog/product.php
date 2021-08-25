c1d77f2dee5a49238ef83c0b2e1aa8c288fa0c9f
nik <nikandr33@inbox.ru>
2021-08-25 16:55:52 +0300

    hotfix

--- upload/admin/controller/catalog/product.php
+++ upload/admin/controller/catalog/product.php
@@ -22,11 +22,7 @@ class ControllerCatalogProduct extends Controller {
 		$this->load->model('catalog/product');
 
 		if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validateForm()) {
-		    echo "<pre>";
-		    print_r($this->request->post);
-		    echo "</pre>";
-
-//			$this->model_catalog_product->addProduct($this->request->post);
+			$this->model_catalog_product->addProduct($this->request->post);
 
 			$this->session->data['success'] = $this->language->get('text_success');
 
